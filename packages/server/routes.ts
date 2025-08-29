8ab3ffabe125d2b5d1da57a2e33a6cca44024f04
Thiago-miotti <thiago.miotti10@gmail.com>
2025-08-29 12:54:47 -0300

    final commit

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,7 +1,6 @@
-import express from 'express';
 import type { Request, Response } from 'express';
+import express from 'express';
 import { chatController } from './controllers/chat.controller';
-import { PrismaClient } from './generated/prisma';
 import { reviewController } from './controllers/review.controller';
 
 const router = express.Router();
@@ -16,6 +15,11 @@ router.get('/api/hello', (req: Request, res: Response) => {
 
 router.post('/api/chat', chatController.sendMessage);
 
-router.get('/api/products/:id/review', reviewController.getReviews);
+router.get('/api/products/:id/reviews', reviewController.getReviews);
+
+router.post(
+   '/api/products/:id/reviews/summarize',
+   reviewController.summarizeReviews
+);
 
 export default router;
--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,8 +1,28 @@
-import { type Review } from '../generated/prisma';
+import { llmClient } from '../llm/client';
+import template from '../prompts/summarize-reviews.txt';
 import { reviewRepository } from '../repositories/review.repository';
 
 export const reviewService = {
-   async getReviews(productId: number): Promise<Review[]> {
-      return reviewRepository.getReviews(productId);
+   async summarizeReviews(productId: number): Promise<string> {
+      const existingSummary =
+         await reviewRepository.getReviewSummary(productId);
+      if (existingSummary) {
+         return existingSummary;
+      }
+
+      const reviews = await reviewRepository.getReviews(productId, 10);
+      const joinedReviews = reviews.map((r) => r.content).join('\n\n');
+      const prompt = template.replace('{{reviews}}', joinedReviews);
+
+      const { text: summary } = await llmClient.generateText({
+         model: 'gpt-4.1',
+         prompt,
+         temperature: 0.2,
+         maxTokens: 500,
+      });
+
+      await reviewRepository.storeReviewSummary(productId, summary);
+
+      return summary;
    },
 };
